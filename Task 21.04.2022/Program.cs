49c8f152cf2d36b922b80fb37c4b893ca41f435f
Sarkhan666 <sarkhanak@code.edu.az>
2022-04-21 03:34:38 +0400

    Add project files.

---
+++ Task 21.04.2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_21._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ Task 21.04.2022/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_21._04._2022
+{
+   internal class Student
+   {
+        public string FullName;
+        private string _GroupNo;
+        public int Age;
+
+        public string No
+        { 
+           get => _GroupNo;
+           set
+            
+          while (!CheckGroupNo(value))
+          {
+                Console.WriteLine("error");
+                value = Console.ReadLine();
+          }
+            _GroupNo = value;
+
+        }
+        static bool CheckGroupNo( string name)
+        {
+            if (name.Length == 4)
+            {
+                if (char.IsUpper(name[0]))
+                {
+                    bool check = false;
+                }
+                  for (int i = 1; i < name.Length; i++)
+                  {
+                    if (char.IsDigit(name[i]))
+                    {
+                        check = true;
+                        return check;
+                    }
+                  }
+                  Console.WriteLine("enter 3 digits");
+            }
+            Console.WriteLine("first letter must be uppercase");
+            Console.WriteLine("group name must contain 4 characters");
+            return true;
+        }
+
+
+
+
+            
+        
+   }
+}
